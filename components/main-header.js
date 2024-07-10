4c2b1ba6941e7448323a2ef5cd90d3903f3c427e
Subhashree Patra <subhashreepatra4u@gmail.com>
2024-07-10 09:26:00 +0530

    Optimize images with Nextjs Image component

--- components/main-header.js
+++ components/main-header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from 'next/image';
 
 import logoImg from "@/assets/logo.png";
 import classes from './main-header.module.css';
@@ -7,7 +8,15 @@ export default function MainHeader(){
     return(
 <header className={classes.header}>
     <Link className={classes.logo} href="/">
-    <img src={logoImg.src} alt="A plate with food on it"/> 
+    {/**Warning is telling(without using priority) that you have a large image that is not being loaded optimally.
+     *  LCP measures the time from when the user initiates loading the page until the largest image or text block is rendered within the viewport.
+     * With images, and other resources, we can use a technique called pre-loading. As soon as the browser hits the preload link, it'll get that resource and continue processing the HTML for the page,
+     * 
+     * In Next.js, the next/image component has a priority prop, which you can activate to trigger the preload of the resource,
+     *   
+     * We  should use the priority property on any image detected as the Largest Contentful Paint (LCP) element. It may be appropriate to have multiple priority images, as different images may be the LCP element for different viewport sizes.
+     */}
+    <Image src={logoImg} alt="A plate with food on it" priority/> 
     NextLevel Food
     </Link>
     <nav className={classes.nav}>
