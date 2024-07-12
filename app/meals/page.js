126933de09c1d61b6d80b1ead9881da6eaa57cb3
Subhashree Patra <subhashreepatra4u@gmail.com>
2024-07-13 02:54:18 +0530

    Output meal data and images with unknown height n width(now no meal data available,check next commit)

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,20 @@
 import Link from "next/link";
 
+import classes from './page.module.css';
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage(){
     return(<>
-    <h1>All meals will be listed here</h1>
+    <header className={classes.header}>
+    <h1>Deliciuous meals created <span className={classes.highlight}>by you</span></h1>
+    <p>Choose your favorite recipe and cook it yourself!</p>
+    <p className={classes.cta}>
+        <Link href="/meals/share">Share your Recipe</Link>
+    </p>
+    </header>
+    <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+    </main>
     <p><Link href="meals/meal-1">Meal 1</Link></p>
     <p><Link href="meals/meal-2">Meal 2</Link></p>
     <p><Link href="/meals/share">Share</Link></p>
---
+++ components/meals/meal-item.js
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import classes from './meal-item.module.css';
+
+
+export default function MealItem({image,title,creator,summary,slug}){
+    return(<article className={classes.meal}>
+        <header>
+            {/** fill is a boolean that causes the image to fill the parent element, which is useful when the width and height are unknown.
+             * The parent element must assign position: "relative", position: "fixed", or position: "absolute" style. 
+             * By default, the img element will automatically be assigned the position: "absolute" style. */}
+            <Image src={image} alt={title} fill />
+            <div>
+                <h2>{title}</h2>
+                <p>by {creator}</p>
+            </div>
+        </header>
+        <div className={classes.content}>
+            <p className={classes.summary}>{summary}</p>
+        </div>
+        <div className={classes.actions}>
+            <Link href={`/meals/${slug}`}>View Details</Link>
+        </div>
+    </article>);
+}
