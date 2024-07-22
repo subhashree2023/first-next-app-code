057a229b19d0ad5618138ca70f9bb20303cc4af6
Subhashree Patra <subhashreepatra4u@gmail.com>
2024-07-22 23:14:06 +0530

    Fetching data  with SQLite and showing meals in page

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 import classes from './page.module.css';
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage(){
+export default async function MealsPage(){
+    const meals=await getMeals();
+    //console.log(meals);
     return(<>
     <header className={classes.header}>
     <h1>Deliciuous meals created <span className={classes.highlight}>by you</span></h1>
@@ -13,10 +16,7 @@ export default function MealsPage(){
     </p>
     </header>
     <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
     </main>
-    <p><Link href="meals/meal-1">Meal 1</Link></p>
-    <p><Link href="meals/meal-2">Meal 2</Link></p>
-    <p><Link href="/meals/share">Share</Link></p>
     </>);
 }
--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -7,10 +7,12 @@ import classes from './meal-item.module.css';
 export default function MealItem({image,title,creator,summary,slug}){
     return(<article className={classes.meal}>
         <header>
+        <div className={classes.image}>
             {/** fill is a boolean that causes the image to fill the parent element, which is useful when the width and height are unknown.
              * The parent element must assign position: "relative", position: "fixed", or position: "absolute" style. 
              * By default, the img element will automatically be assigned the position: "absolute" style. */}
-            <Image src={image} alt={title} fill />
+            <Image src={image} alt={title} fill/>
+            </div>
             <div>
                 <h2>{title}</h2>
                 <p>by {creator}</p>
