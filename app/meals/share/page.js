efa911b224c20d751422a72a023d69966d80a12e
Subhashree Patra <subhashreepatra4u@gmail.com>
2024-08-08 00:50:57 +0530

    Redirect to meals page after submit and managing form submission status with useFormStatus

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,12 @@
 /**if URL: http://localhost:3000/meals/share, then this page will be rendered */
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/action';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+
     /**Now if we want to make this page to client component(by using 'use client' on top) then it will throw an error that we can't define server action inside client component.
      * To use it inside client component,we can either export then from a separate file(with 'use server' on top of that file) or pass them through props from server component.
      * and here we have JsX code ,so to make this page lean and clean ,form submission handling logic might be in a different file(here action.js). 
@@ -48,7 +51,11 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image"/>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            {/**Here i want to show submitting... text when data are submitting in place of Share meal.For that we need to know submission status of the form,that we can get using react's useFormStatus hook(this hook can be used with next Js only),
+             * but this hook will give submission status of form if it's inside of that form for which it should give the status,means inside of some component inside of that form.
+             * again useFormStatus is a react hook,so it will work on client component.So we will create a client componet and change button text conditionally on status change and render that component here 
+             */}
+            <button>Share Meal</button>
           </p>
         </form>
       </main>
