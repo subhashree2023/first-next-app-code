0e27e2b824fb2ff1237d5c36a9280bb229299898
Subhashree Patra <subhashreepatra4u@gmail.com>
2024-08-14 03:03:11 +0530

    Static metadata use

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,17 @@ import classes from './page.module.css';
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
+/**metadata object from any layout.js or page.js file to add additional page information like title and description.
+ *  Any metadata in layout.js will be inherited by all pages that use it.
+ * Next.js will automatically add the title and metadata to your application.
+ * But if you want to add a custom title for a specific page then You can do this by adding a metadata object
+ *  to the page itself. Metadata in nested pages will override the metadata in the parent. 
+ * This is static metadata defining method.Here meal page metadata will override layout one.*/
+export const metadata = {
+  title: 'All Meals',
+  description: 'Delicious meals, shared by a food-loving community.',
+};
+
 /**if we use loading.js ,whole page content renders after getting meals data but here header doesn't depend on meal data,so it should render on page load.
  * Suspense is a component provided by React that allows you to handle loading states and show fallback content until some data or resource has been loaded.
  * So whenever we have a component like Meals(),which performs some data fetching and returns such a promise here, such components will trigger suspense
